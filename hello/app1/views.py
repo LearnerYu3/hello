457c82ace47021876339d032b2594dacb2e44967
a-real-idiot <1330286215@qq.com>
2020-11-21 20:50:39 +0800

    django

---
+++ hello/app1/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request, "app1/index.html")
+
+def brian(request):
+    return HttpResponse("Hello, Brian!")
+
+def ycy(request):
+    return HttpResponse("Hello, ycy!")
+
+def greet(request, name):
+    return render(request, "app1/greet.html", {
+        "name": name.capitalize()
+    })
